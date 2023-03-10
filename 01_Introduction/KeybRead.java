3301d3d4b863a75e101e270a1f5abc0e2d033310
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-10 21:05:35 +0530

    Scanner class to take a string input

--- 01_Introduction/KeybRead.java
+++ 01_Introduction/KeybRead.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class KeybRead {
     public static void main(String[] args) {
 
-        // For Integer
+        // For Integer and String
+        String name;
         int a, b, c;
+
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter Two nos.");
+
+        System.out.print("Enter Your Name: ");
+        name = s.nextLine();
+        
+
+        System.out.print("Enter Two nos.: ");
         a = s.nextInt();
         b = s.nextInt();
         c = a + b;
+
+        System.out.println("Welcome "+ name);
         System.out.println("Sum is " +c);
 
     }
