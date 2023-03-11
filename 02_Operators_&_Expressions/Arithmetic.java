a953eac95ac4133eceac50753ad5f8bbc5672cc4
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-11 23:45:03 +0530

    Arithmetic Operations

---
+++ 02_Operators_&_Expressions/Arithmetic.java
@@ -0,0 +1,11 @@
+
+public class Arithmetic {
+    public static void main(String args[]) {
+        int a = 20, b = 10, c, d;
+        c = a / b;
+        d = a % b;
+        System.out.println("Division is "+c);
+        System.out.println("Modulus is "+d);
+
+    }
+}
