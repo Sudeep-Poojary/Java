b45d62dab26e3b9c11a91c48c2f69456f2fd9d0e
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-11 23:50:31 +0530

    Performed Arithmetic Operations

--- 02_Operators_&_Expressions/Arithmetic.java
+++ 02_Operators_&_Expressions/Arithmetic.java
@@ -1,11 +1,17 @@
 
 public class Arithmetic {
     public static void main(String args[]) {
-        int a = 20, b = 10, c, d;
-        c = a / b;
-        d = a % b;
-        System.out.println("Division is "+c);
-        System.out.println("Modulus is "+d);
+        int num1 = 20, num2 = 10, add, sub, mul, div, mod;
+        add = num1 + num2;
+        sub = num1 - num2;
+        mul = num1 * num2;
+        div = num1 / num2;
+        mod = num1 % num2;
+        System.out.println("Addition is "+add);
+        System.out.println("Subtraction is "+sub);
+        System.out.println("Multiplication is "+mul);
+        System.out.println("Division is "+div);
+        System.out.println("Modulus is "+mod);
 
     }
 }
