9579be51a5b3629363ef67f493774a598deca080
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-12 16:41:38 +0530

    taking nos. as input from user

--- 02_Operators_&_Expressions/Arithmetic.java
+++ 02_Operators_&_Expressions/Arithmetic.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
 
 public class Arithmetic {
     public static void main(String args[]) {
-        int num1 = 20, num2 = 10, add, sub, mul, div, mod;
+        int num1, num2, add, sub, mul, div, mod;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first no.: ");
+        num1 = scan.nextInt();
+
+        System.out.print("Enter the second no.: ");
+        num2 = scan.nextInt();
+
         add = num1 + num2;
         sub = num1 - num2;
         mul = num1 * num2;
         div = num1 / num2;
         mod = num1 % num2;
-        System.out.println("Addition is "+add);
-        System.out.println("Subtraction is "+sub);
-        System.out.println("Multiplication is "+mul);
-        System.out.println("Division is "+div);
-        System.out.println("Modulus is "+mod);
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("Addition of " +num1+ " and " +num2+ " is " +add);
+        System.out.println("Subtraction of " +num1+ " and " +num2+ " is "+sub);
+        System.out.println("Multiplication of " +num1+ " and " +num2+ " is "+mul);
+        System.out.println("Division of " +num1+ " and " +num2+ " is " +div);
+        System.out.println("Modulus of " +num1+ " and " +num2+ " is " +mod);
 
     }
 }
