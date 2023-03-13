4fde34dc06cca3e1d0bc66b2a90f37459c8bbb0e
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-13 20:49:31 +0530

    Inc and Dec Operations

---
+++ 02_Operators_&_Expressions/IncDec.java
@@ -0,0 +1,13 @@
+public class IncDec {
+    public static void main(String[] args) {
+        int x = 5, y, z;
+
+        y = x++;
+
+        z = ++x;
+
+        System.out.println(y);
+        System.out.println(z);
+
+    }
+}
