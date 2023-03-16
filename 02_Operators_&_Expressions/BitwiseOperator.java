44d08c32793bff81cdb5f3da296c57fe5740a1fe
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-16 20:57:06 +0530

    Bitwise Operations

--- 02_Operators_&_Expressions/BitwiseOperator.java
+++ 02_Operators_&_Expressions/BitwiseOperator.java
@@ -1,7 +1,11 @@
 public class BitwiseOperator {
     public static void main(String[] args) {
-        int x = 10, y = 6, z;
+        int x = 10, y = 6, z, a, b;
          z = x & y;
+         a = x | y;
+         b = x ^ y;
          System.out.println(z);
+         System.out.println(a);
+         System.out.println(b);
     }
 }
