7f3492393cbf764e4ce52846ad15a1f80b93ce8c
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-06-26 00:37:33 +0530

    Print Tables using function

---
+++ 06_Functions/Lecture7_5.java
@@ -0,0 +1,22 @@
+// Print tables
+
+import java.util.*;
+
+public class Lecture7_5 {
+
+    public static void printTables(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+
+        return;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        printTables(n);
+    }
+}
